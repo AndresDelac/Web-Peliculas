92c29c40fcff2f6858af3de3d3deee48dafe7e4d
AndresDelac23 <hugoandres2205@gmail.com>
2024-03-04 20:14:48 -0500

    Estructura Html, css y js

--- front/scripts/tempData.mjs
+++ front/scripts/tempData.mjs
@@ -30,3 +30,6 @@ const tempData = [
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
 ];
+
+
+export default tempData;
